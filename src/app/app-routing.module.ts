11f848e9c78133ec5b8f7ec59716e4456ccf8b56
Eva Berbel <berbelev@gmail.com>
2023-01-05 01:03:33 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // idicar objeto ruta
+  //TODO:buscar info al respecto o preguntar al profe
+  {path:'',redirectTo:'AppComponent',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
